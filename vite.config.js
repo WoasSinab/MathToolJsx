c9ed8d66b199808c3c7923dd5bf143641fc6607d
Sinab <mrsaher83@gmail.com>
2025-02-10 13:19:02 +0330

    edited

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MathToolJsx/"
+  base: "/MathToolJsx/",
+  build: {
+    rollupOptions: {
+      external: ["react-router-dom"]
+    }
+  }
 });
