f251799e7c3eb4106e3c09a87a11105c082e0b02
Sinab <mrsaher83@gmail.com>
2025-02-14 10:15:15 +0330

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './assets/styles.css';
 
 function App() {
   return (
-    <Router basename="/MathToolJsx/"> {/* اضافه کردن basename */}
+    <Router basename="/MathToolJsx/">
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/math-game" element={<MathGame />} />
